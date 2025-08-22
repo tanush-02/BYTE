c22c6415592ef750224e61b1e825e548439e6c16
Tanush <tanushr20@gmail.com>
2025-08-22 20:47:04 +0530

    Updated frontend and Initialised Backend

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 3002;
+const uri = process.env.MONGO_URL;
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+
+
+
+mongoose.connect(uri)
+  .then(() => {
+    console.log("✅ MongoDB connected");
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.error("❌ MongoDB connection failed:", err.message);
+  });
