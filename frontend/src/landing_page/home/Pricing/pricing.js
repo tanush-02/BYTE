e74fa124d98f4945302a43066e5d5d2ae8b28ad5
Tanush <tanushr20@gmail.com>
2025-08-23 12:52:53 +0530

    Task 2: Implemented Verified Products

--- frontend/src/landing_page/home/Pricing/pricing.js
+++ frontend/src/landing_page/home/Pricing/pricing.js
@@ -1,6 +1,18 @@
 export default function Pricing() {
   return(
+     
 <div class="cardall p-4">
+  <div class="marquee-wrapper">
+  <div class="marquee-content">
+    <span>Products Verified by KISAAN</span>
+     <span>🛒 Shop Now! &nbsp;</span>
+    <span>💰 Best Prices! &nbsp;</span>
+    <span>🌾 Top Quality! &nbsp;</span>
+    <span>🌱 Fresh Products! &nbsp;</span>
+    <span>💡 Verified! &nbsp;</span>
+  </div>
+</div>
+
   <div class="card mt-5">
     {/* <img src="npk.png" class="card-img-top" alt="IFFCO Urea Fertilizer"> */}
     <div class="card-body ">
@@ -14,7 +26,7 @@ export default function Pricing() {
     {/* <img src="zincDap.png" class="card-img-top" alt="Zinc DAP Fertilizer"> */}
     <div class="card-body">
       <h5 class="card-title">Zinc DAP &nbsp;&nbsp;<i class="fa-regular fa-circle-check"></i>Verified</h5>
-      <p class="card-text">Jai Kisaan 24:24:00 is an FCO registered NP nutrient source fertilizer. It contains a minimum of 24.0% Nitrogen and 24.0% Phosphorous as P2O5 by weight.</p>
+      <p class="card-text">Urea is a pure source of Nitrogen (N), with a grade of 46-0-0, meaning it contains a minimum of 46% Nitrogen by weight.Nitrogen from urea is essential for promoting lush, leafy vegetative growth. It is a fundamental building block for proteins and chlorophyll, which is vital for photosynthesis..</p>
       <a href="https://jaikisaan.online/fertilizer?location=bharat-npk-19-19-19" class="btn btn-success">Buy Now</a>
     </div>
   </div>
